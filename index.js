1b80a4d320ee7d77828a0b91f6a610711ab65ef5
Ivan Fadeev <ifad6@yandex.ru>
2018-04-27 23:49:32 +0300

    Первичные правки

---
+++ index.js
@@ -0,0 +1,30 @@
+var canvas = document.getElementById('example');
+var ctx = canvas.getContext('2d');
+ctx.lineCap = 'round';
+var l = 0;
+var frameRate = 30;
+
+function scale() {
+
+	canvas.width = document.body.clientWidth;
+	canvas.height = document.body.clientHeight;
+
+	l = Math.round(canvas.height / 12);
+	ctx.lineWidth = Math.round(l / 3);
+	ctx.lineCap = 'round';
+
+}
+scale();
+
+$(document).keydown(function(e){
+
+	//e.preventDefault();
+
+	switch(e.keyCode) {
+		case 37: elx = elx - l*2 * Math.round(Math.cos(15)); hlx = elx - Math.round(l*2/3);
+		break;
+		case 39: elx = elx + l*2 * Math.round(Math.cos(30)); hlx = elx + Math.round(l/6);
+		break;
+	}
+
+});
